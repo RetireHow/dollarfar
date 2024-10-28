110869e782bab9a11c3a9ecf5c5124acac3c57fc
billalhossain3 <billal@skygoalnext.com>
2024-10-28 17:17:44 +0600

    solved typescript related issue

--- src/pages/BudgetCalculator/Stepper.tsx
+++ src/pages/BudgetCalculator/Stepper.tsx
@@ -1,36 +1,6 @@
-import React from "react";
-import type { StepsProps } from "antd";
-import { Steps, Typography } from "antd";
+export default function Stepper() {
+  return (
+    <div>Stepper</div>
+  )
+}
 
-const customDot: StepsProps["progressDot"] = (dot, {index }) => (
-  <Typography
-  className="border-[1px] border-gray-500 rounded-full w-[30px] h-[30px] flex justify-center items-center font-medium text-[1.2rem]"
-  >
-    {index + 1}
-  </Typography>
-);
-
-const Stepper: React.FC = () => (
-  <Steps
-    className="mb-[3rem]"
-    current={1}
-    percent={0}
-    progressDot={customDot}
-    items={[
-      {
-        title: "",
-      },
-      {
-        title: "",
-      },
-      {
-        title: "",
-      },
-      {
-        title: "",
-      },
-    ]}
-  />
-);
-
-export default Stepper;
