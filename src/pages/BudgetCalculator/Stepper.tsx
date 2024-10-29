5ee9ef427d25f4f943ae23e671001c8149372b96
billalhossain3 <billal@skygoalnext.com>
2024-10-29 17:12:01 +0600

    remove next function from the step

--- src/pages/BudgetCalculator/Stepper.tsx
+++ src/pages/BudgetCalculator/Stepper.tsx
@@ -1,15 +1,11 @@
-import {
-  goToStep,
-} from "../../redux/features/stepperSlice/stepperSclie";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { useAppSelector } from "../../redux/hooks";
 
 export const steps = [1, 2, 3, 4, 5, 6, 7];
 
 export default function Stepper() {
-  const dispatch = useAppDispatch();
   const activeStep = useAppSelector((state) => state.stepper.activeStep);
 
-   // Calculate width percentage for completed steps
+  // Calculate width percentage for completed steps
   //  const progressWidth = (activeStep / (steps.length - 1)) * 100;
 
   return (
@@ -18,7 +14,6 @@ export default function Stepper() {
         {steps.map((step, index) => (
           <div
             key={index}
-            onClick={() => dispatch(goToStep(index))}
             className={`w-[2.5rem] h-[2.5rem] rounded-full border-[1px] border-gray-500 flex justify-center items-center font-extrabold text-[1.25rem] ${
               activeStep === index ? "bg-gray-800 text-white" : "bg-white"
             }`}
